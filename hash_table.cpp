cf03fb30913636d2ee8c0999d7dfc64f5ee69a26
codechodetoad <229386127+phamdop1-bit@users.noreply.github.com>
2025-10-17 01:28:33 -0400

    added first version of usecase and member  method and tostring method

--- hash_table.cpp
+++ hash_table.cpp
@@ -39,3 +39,52 @@ T Element<T>::get_data() {
     return data;
 }
 
+/*
+Precondition: Valid data and key provided
+Postcondition: Returns true if element with data and key exists, false otherwise
+*/
+template<typename T>
+bool HashTable<T>::member(T data, int key) {
+    int index = key % size;
+    Element<T>* current = table[index];
+    
+    // Traverse the chain at this index
+    while (current != nullptr) {
+        if (current->get_key() == key && current->get_data() == data) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+/*
+Precondition: HashTable exists
+Postcondition: Returns string representation of the hash table
+*/
+template<typename T>
+string HashTable<T>::to_string() {
+    stringstream ss;
+    
+    for (int i = 0; i < size; i++) {
+        ss << i << ":";
+        
+        Element<T>* current = table[i];
+        bool first = true;
+        
+        while (current != nullptr) {
+            if (!first) {
+                ss << " ";
+            }
+            ss << "(" << current->get_data() << "," << current->get_key() << ")";
+            first = false;
+            current = current->next;
+        }
+        
+        if (i < size - 1) {
+            ss << "\n";
+        }
+    }
+    
+    return ss.str();
+}
