17fc0c58971c05c263a0265b76635f1cc96a665f
WilliamWang777 <189502003+WilliamWang778@users.noreply.github.com>
2025-10-18 00:17:52 -0400

    added the test case in main function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,67 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#include "hash_table.cpp"
+#include "hash_table.h"
+#include "usecase.cpp"
 
 int main(){
 
+    const string csv = "logins.csv"; 
+
+    const int m = 10;
+
+    HashTable<string>* ht = create_table<string>(csv, m);
+
+    if (ht == nullptr){
+        cerr << "Failed to create hash table.\n";
+        return 1;
+    }
+
+    string username1 = "EaQrMZZQisY";
+    string password1 = "2091698288";
+    string username2 = "fake-user";
+    string password2 = "1122334455";
+
+    bool check1 = login(ht, username1, password1);
+    bool check2 = login(ht, username2, password2);
+
+    if (check1) {
+        cout << "(" << username1 << "," << password1 << ") : granted\n";
+    } else {
+        cout << "(" << username1 << "," << password1 << ") : denied\n";
+    }
+
+    if (check2) {
+        cout << "(" << username2 << "," << password2 << ") : granted\n";
+    } else {
+        cout << "(" << username2 << "," << password2 << ") : denied\n";
+    }
 
 
 
+    cout << "\n=== Login ===\n";
+    cout << "Enter username: ";
+    string user;
+    if (! getline(cin, user)) return 0;
 
+    cout << "Enter password: ";
+    string pass;
+    if (! getline(cin, pass)) return 0;
 
+    if (login(ht, user, pass)) {
+        cout << "Access granted\n";
+    } else {
+        cout << "Access denied\n";
+    }
 
-
-
+    delete ht;
     return 0;
 }
+
+
+
+
+
+
+
+
