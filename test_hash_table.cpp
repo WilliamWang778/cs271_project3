5199657878aaa4c54cd05aa9a7dba7096c4e652e
WilliamWang777 <189502003+WilliamWang778@users.noreply.github.com>
2025-10-18 14:30:16 -0400

    created a test_hash_table.cpp with the structure.

---
+++ test_hash_table.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include "usecase.cpp"
+
+
+using namespace std;
+
+
+
+
+
+void test_get_key()
+{
+  
+
+
+
+
+}
+
+
+
+
+void test_get_data()
+{
+ 
+
+
+
+}
+
+
+
+
+
+void test_insert()
+{
+   
+
+
+
+}
+
+
+
+
+
+void test_remove()
+{
+
+
+    
+   
+}
+
+
+
+
+
+
+void test_member()
+{
+    
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+void test_login()
+{
+    
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+int main()
+{
+    string file_name = "usecase.cpp";
+    cout << endl
+         << "Running tests for " << file_name << endl
+         << endl;
+
+    test_get_key();
+    test_get_data();
+    test_insert();
+    test_remove();
+    test_member();
+    test_login();
+
+    cout << "Testing completed" << endl;
+
+    return 0;
+}
